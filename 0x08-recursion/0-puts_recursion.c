c5ff695c32353f4e946cedf3c72cb92e39cdbbc5
Calembe <calebiteba@gmail.com>
2023-08-30 14:07:42 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,12 @@
  * @s: input
  * Return: Always 0 (success)
  */
-void _puts_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 		if (*s)
 	{
 			_putchar(*s);
-			_puts_rev_recursion(s + 1);
+			_puts_recursion(s + 1);
 	}
 		else
 			_putchar('\n');
